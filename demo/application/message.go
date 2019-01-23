d0f5a7c5ae257ca244021b6a25ec541bfd0eb6e8
mats9693 <982254249@qq.com>
2019-01-23 18:20:30 +0800

    go -> js succeed.

--- demo/application/message.go
+++ demo/application/message.go
@@ -7,7 +7,12 @@ import (
 
 func handleMessages(_ *astilectron.Window, m bootstrap.MessageIn) (payload interface{}, err error) {
 	switch m.Name {
-	case "m.Name":
+	case "Account":
+		if string(m.Payload) == "administrator" {
+			payload = "0x123456789012345678901234567890"
+			return
+		}
 	}
+	payload = err.Error()
 	return
 }
