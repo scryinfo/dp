36012af41cb1345f423c1e808fda4d4df4b317ad
smallfat <hq.liu@qq.com>
2019-01-30 09:29:11 +0800

    contract unit test

--- demo/contracts/migrations/2_deploy_contracts.js
+++ demo/contracts/migrations/2_deploy_contracts.js
@@ -10,11 +10,12 @@ module.exports = function(deployer, network, accounts) {
     tokenContract.transfer(accounts[1], 10000000)
     tokenContract.transfer(accounts[2], 10000000)
     tokenContract.transfer(accounts[3], 10000000)
-    return tokenContract.transfer(accounts[4], 10000000)
+    tokenContract.transfer(accounts[4], 10000000)
+    return tokenContract.transfer(accounts[5], 10000000)
   }).then(function(result) {
     return tokenContract.balanceOf.call(accounts[0])
   }).then(function(balance) {
-    console.log("balance of account2", accounts[0], balance) 
+    console.log("balance of account0", accounts[0], balance) 
   }) 
   
 };
