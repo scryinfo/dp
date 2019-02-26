0071105fead66e6ef6f66aa5ecfe9888a14dee65
mats9693 <982254249@qq.com>
2019-02-26 10:25:16 +0800

    app optimize

---
+++ demo/src/application/resources/app/src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import VueRouter from 'vue-router'
+import store from './vuex/store'
+import Vuex from 'vuex'
+import routes from './routes'
+
+Vue.use(ElementUI)
+Vue.use(VueRouter)
+Vue.use(Vuex)
+
+const router = new VueRouter({
+  routes
+})
+
+new Vue({
+  //el: '#app',
+  //template: '<App/>',
+  router,
+  store,
+  //components: { App }
+  render: h => h(App)
+}).$mount('#app')
+
