6e50e6d6b62be238245aa2a5b1fc27d3e3a48984
smallfat <hq.liu@qq.com>
2019-03-02 17:26:13 +0800

    add unit test into sdkinterface_test

--- demo/src/application/sdkinterface/sdkinterface_test.go
+++ demo/src/application/sdkinterface/sdkinterface_test.go
@@ -1,8 +1,10 @@
 package sdkinterface
 
 import (
+    "github.com/scryinfo/iscap/demo/src/application/definition"
     "math/big"
     "testing"
+    "time"
 )
 
 func TestInitialize(t *testing.T) {
@@ -12,13 +14,49 @@ func TestInitialize(t *testing.T) {
     }
 }
 
+//notice: deployer_keyjson and deployer_password \
+//need to be changed to yours before testing
 func TestTransferTokenFromDeployer(t *testing.T) {
     Initialize()
-
     CreateUserWithLogin("111111")
-
-    err := TransferTokenFromDeployer(big.NewInt(100))
+    err := TransferTokenFromDeployer(big.NewInt(40000000))
     if err != nil {
         t.Fail()
     }
+}
+
+func TestUserLogin(t *testing.T) {
+    Initialize()
+    CreateUserWithLogin("111111")
+    login, err := UserLogin(curUser.Account.Address, "111111")
+    if err != nil {
+        t.Fail()
+    }
+
+    if !login {
+        t.Fail()
+    }
+}
+
+
+func TestPublishData(t *testing.T) {
+    Initialize()
+    CreateUserWithLogin("111111")
+    TransferTokenFromDeployer(big.NewInt(40000000))
+
+    proofData := []string{"proof1", "proof2", "proof3"}
+    data := definition.PubData{
+        "i am a solid man",
+        proofData,
+        "evil data",
+        10000,
+        "seller",
+    }
+
+    _, err := PublishData(data, "111111")
+    if err != nil {
+        t.Fail()
+    }
+
+    time.Sleep(100000000)
 }
