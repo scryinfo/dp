09c02336cbf75607d5c05f1bbd3528283bc06500
mats9693 <982254249@qq.com>
2019-03-27 10:48:03 +0800

    for review contract

--- demo/src/application/build.ps1
+++ demo/src/application/build.ps1
@@ -2,18 +2,25 @@ $WorkPath = $PSScriptRoot
 cd $WorkPath
 Remove-Item .\UI\resources\app\* -Recurse
 
+$AppName = (Get-Content ".\bundler.json" -TotalCount 2)[-1].Split(":")[1].Split("`"")[1]
+echo " * Name in bundle file: " $AppName
+
 cd .\UI
-echo "Vue.js build prepared. "
+echo " * Vue.js build prepared. "
 Start-Sleep -Milliseconds 2000
 
 npm run build
+echo " * Copy UI files to UI directory: "
 Remove-Item ..\resources\app -Recurse
 Copy-Item .\resources\app ..\resources -Recurse
-Remove-Item "C:\Users\马同帅\AppData\Roaming\My Astilectron demo\resources\app" -Recurse
-Copy-Item .\resources\app "C:\Users\马同帅\AppData\Roaming\My Astilectron demo\resources" -Recurse
+echo " * finish." 
+echo " * Copy UI files to bundle directory. "
+Remove-Item "C:\Users\马同帅\AppData\Roaming\$AppName\resources\app" -Recurse
+Copy-Item .\resources\app "C:\Users\马同帅\AppData\Roaming\$AppName\resources" -Recurse
+echo " * finish. "
 
 cd ..
-echo "Go-astilectron bundle prepared. "
+echo " * Go-astilectron bundle prepared. " 
 Start-Sleep -Milliseconds 2000
 
 astilectron-bundler -v
