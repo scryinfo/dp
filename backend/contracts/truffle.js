c09a9e838321ad6790b9248b94c09b5385b2b982
mats9693 <982254249@qq.com>
2019-06-20 10:52:24 +0800

    verification lib

--- backend/contracts/truffle.js
+++ backend/contracts/truffle.js
@@ -22,7 +22,8 @@ module.exports = {
 	  development: {
 		  host: '127.0.0.1',
 		  port: '7545',
-		  network_id: '*'
+		  network_id: '*',
+		  gas: 100000000 // 10 eth
 	  },
 
 	  geth: {
@@ -31,4 +32,11 @@ module.exports = {
 		  network_id: '*'
 	  }
   }
+
+  // solc: {
+  //     optimizer: {
+  //         enabled: true,
+  //         runs: 200
+  //     }
+  // }
 };
