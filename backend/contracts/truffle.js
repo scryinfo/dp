f00167d45f897aee71329f9a511a662fd074befb
mats9693 <982254249@qq.com>
2019-06-21 11:12:32 +0800

    modify enum type in verification

--- backend/contracts/truffle.js
+++ backend/contracts/truffle.js
@@ -23,6 +23,7 @@ module.exports = {
 		  host: '127.0.0.1',
 		  port: '7545',
 		  network_id: '*'
+		  // gas: 100000000 // 10 eth
 	  },
 
 	  geth: {
@@ -30,12 +31,12 @@ module.exports = {
 		  port: '8545',
 		  network_id: '*'
 	  }
-  }
+  },
 
-  // solc: {
-  //     optimizer: {
-  //         enabled: true,
-  //         runs: 200
-  //     }
-  // }
+  solc: {
+      optimizer: {
+          enabled: true,
+          runs: 200
+      }
+  }
 };
