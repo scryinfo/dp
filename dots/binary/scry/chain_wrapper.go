b4121bd6a07ad1d25ace853b77b70733b2edfbed
smallfat <hq.liu@qq.com>
2019-06-28 09:57:40 +0800

    use 4 spaces indent instead of tab

--- dots/binary/scry/chain_wrapper.go
+++ dots/binary/scry/chain_wrapper.go
@@ -4,7 +4,7 @@
 package scry
 
 import (
-	"github.com/ethereum/go-ethereum/common"
+    "github.com/ethereum/go-ethereum/common"
     "github.com/ethereum/go-ethereum/ethclient"
     tx "github.com/scryinfo/dp/dots/eth/transaction"
     "math/big"
@@ -12,17 +12,17 @@ import (
 
 type ChainWrapper interface {
     Conn() *ethclient.Client
-	Publish(txParams *tx.TxParams, price *big.Int, metaDataID []byte, proofDataIDs []string,
-		proofNum int, detailsID string, supportVerify bool) (string, error)
-	PrepareToBuy(txParams *tx.TxParams, publishId string, startVerify bool) error
-	BuyData(txParams *tx.TxParams, txId *big.Int) error
-	CancelTransaction(txParams *tx.TxParams, txId *big.Int) error
-	SubmitMetaDataIdEncWithBuyer(txParams *tx.TxParams, txId *big.Int, encyptedMetaDataId []byte) error
-	ConfirmDataTruth(txParams *tx.TxParams, txId *big.Int, truth bool) error
-	ApproveTransfer(txParams *tx.TxParams, spender common.Address, value *big.Int) error
-	Vote(txParams *tx.TxParams, txId *big.Int, judge bool, comments string) error
-	RegisterAsVerifier(txParams *tx.TxParams) error
-	CreditsToVerifier(txParams *tx.TxParams, txId *big.Int, index uint8, credit uint8) error
-	TransferTokens(txParams *tx.TxParams, to common.Address, value *big.Int) error
-	GetTokenBalance(txParams *tx.TxParams, owner common.Address) (*big.Int, error)
+    Publish(txParams *tx.TxParams, price *big.Int, metaDataID []byte, proofDataIDs []string,
+        proofNum int, detailsID string, supportVerify bool) (string, error)
+    PrepareToBuy(txParams *tx.TxParams, publishId string, startVerify bool) error
+    BuyData(txParams *tx.TxParams, txId *big.Int) error
+    CancelTransaction(txParams *tx.TxParams, txId *big.Int) error
+    SubmitMetaDataIdEncWithBuyer(txParams *tx.TxParams, txId *big.Int, encyptedMetaDataId []byte) error
+    ConfirmDataTruth(txParams *tx.TxParams, txId *big.Int, truth bool) error
+    ApproveTransfer(txParams *tx.TxParams, spender common.Address, value *big.Int) error
+    Vote(txParams *tx.TxParams, txId *big.Int, judge bool, comments string) error
+    RegisterAsVerifier(txParams *tx.TxParams) error
+    CreditsToVerifier(txParams *tx.TxParams, txId *big.Int, index uint8, credit uint8) error
+    TransferTokens(txParams *tx.TxParams, to common.Address, value *big.Int) error
+    GetTokenBalance(txParams *tx.TxParams, owner common.Address) (*big.Int, error)
 }
