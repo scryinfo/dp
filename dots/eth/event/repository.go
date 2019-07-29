ff406d508d0e43082e117c701c26ebf6a2eec0a5
smallfat <hq.liu@qq.com>
2019-07-29 13:10:09 +0800

    code refactor

--- dots/eth/event/repository.go
+++ dots/eth/event/repository.go
@@ -10,12 +10,10 @@ import (
 type Callback func(event Event) bool
 
 type Repository struct {
-    //MapEventCallback map[string]map[common.Address]Callback
     MapEventCallback sync.Map
 }
 
 func NewRepository() *Repository {
     return &Repository{
-        //MapEventCallback: make(map[string]map[common.Address]Callback),
     }
 }
