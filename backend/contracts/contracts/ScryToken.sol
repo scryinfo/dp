20275a59ca2cd75281e0522c05a31b1b486f1621
qyzxcswbll <136552740@qq.com>
2019-09-26 14:13:50 +0800

    update contract

--- backend/contracts/contracts/ScryToken.sol
+++ backend/contracts/contracts/ScryToken.sol
@@ -10,10 +10,10 @@ contract ScryToken is StandardToken {
     string public name = "ScryToken";
     string public symbol = "yyy";
     uint8 public decimals = 2;
-    uint256 public INITIAL_SUPPLY = 1000000000;
+//    uint256 public INITIAL_SUPPLY = 1000000000;
 
     constructor () public {
-        totalSupply_ = INITIAL_SUPPLY;
-        balances[msg.sender] = INITIAL_SUPPLY;        
+        totalSupply_ = 1000000000;
+        balances[msg.sender] = 1000000000;
     }
 }
