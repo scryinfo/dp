20e5b05829a952c86d8257c7adf323412b1dab03
Zealy <ljg_cqu@126.com>
2019-11-20 14:42:39 +0800

    Test panics on getStorer

--- dots/eth/nonce/storer_test.go
+++ dots/eth/nonce/storer_test.go
@@ -14,7 +14,7 @@ func Test_Storer_on_PostgreSQL(t *testing.T) {
 	t.Run("main_successful_scenarios", func(t *testing.T) {
 		require := require.New(t)
 
-		// Connect the database
+		// Test connecting the database
 		driverName := "postgres"
 		dataSourceName := "host=localhost port=5432 user=postgres password=QrfV2_Pg sslmode=disable dbname=ljg_cqu"
 		var storer storer
@@ -22,43 +22,72 @@ func Test_Storer_on_PostgreSQL(t *testing.T) {
 			storer = getStorer(driverName, dataSourceName)
 		}, "getStorer(%v, %v) shouldn't panic.\n", driverName, dataSourceName)
 
-		// Create an account if it does not exist yet.
+		// Test creating an account if it does not exist yet.
 		address := "0x99ad45666e02a89eaf06780d6cbd7ac9bb43f643"
 		nonceIn := uint64(100)
 		err := storer.createAccountIfNotExists(address, nonceIn)
 		require.NoErrorf(err, "createAccountIfNotExists(%v, %v) shouldn't return err '%v'\n", address, nonceIn, err)
 
-		// Retrieve nonce at an address
+		// Test retrieving nonce at an address
 		nonceGot, err := storer.retrieveNonceAt(address)
 		require.NoErrorf(err, "retrieveNonceAt(%v) shouldn't return err '%v'\n", address, err)
 		require.Equalf(nonceIn, nonceGot, "retrieveNonceAt(%v), expected %v, but got %v\n ", address, nonceIn, nonceGot)
 
-		// Repeat retrieving nonce, expecting an incremented nonce.
+		// Test repeating retrieving nonce, expecting an incremented nonce.
 		nonceGot2, err := storer.retrieveNonceAt(address)
 		require.NoErrorf(err, "retrieveNonceAt(%v) shouldn't return err '%v'\n", address, err)
 		require.Equalf(nonceGot, nonceGot2-1, "retrieveNonceAt(%v), expected %v, but got %v\n ", address, nonceGot+1, nonceGot2)
 
-		// Restore nonce
+		// Test restoring nonce
 		err = storer.restoreNonce(address, nonceGot2)
 		require.NoErrorf(err, "restoreNonce(%v, %v) shouldn't return err '%v'\n", address, nonceGot2, err)
 
-		// Retrieve nonce again, expecting the nonce just restored.
+		// Test retrieving nonce again, expecting the nonce just restored.
 		nonceGot, err = storer.retrieveNonceAt(address)
 		require.NoErrorf(err, "retrieveNonceAt(%v) shouldn't return err '%v'\n", address, err)
 		require.Equalf(nonceGot2, nonceGot, "retrieveNonceAt(%v), expected %v, but got %v\n ", address, nonceGot2, nonceGot)
 
-		// Reset nonce
+		// Test reseting nonce
 		resetNonce := uint64(90)
 		err = storer.resetNonceAt(address, resetNonce)
 		require.NoErrorf(err, "resetNonceAt(%v, %v) shouldn't return err '%v'\n", address, resetNonce, err)
 
-		// Retrieve nonce again, expecting the value just reset.
+		// Test retrieving nonce again, expecting the value just reset.
 		nonceGot, err = storer.retrieveNonceAt(address)
 		require.NoErrorf(err, "retrieveNonceAt(%v) shouldn't return err '%v'\n", address, err)
 		require.Equalf(resetNonce, nonceGot, "retrieveNonceAt(%v), expected %v, but got %v\n ", address, resetNonce, nonceGot)
 
-		// Close the storer
+		// Test closing the storer
 		err = storer.close()
 		require.NoErrorf(err, "storer shouldn't return err '%v'\n", err)
+	})
+
+	// Test exceptional situations
+
+	// Test panics on getStorer
+	t.Run("panics_on_getStorer", func(t *testing.T) {
+		require := require.New(t)
+
+		// Test connecting the database with unkown driver and inaccessible data source inputs.
+		scenarios := []struct{
+			desc string
+			inDriveName string
+			inDataSourceName string
+		} {
+			{
+				desc: "unknown driver",
+				inDriveName: "sqlite",
+				inDataSourceName: ""},
+			{
+				desc: "inaccessible data source",
+				inDriveName: "postgres",
+				inDataSourceName: ""},
+
+		}
+		for _, scenario := range scenarios {
+			require.Panicsf(func() {
+				getStorer(scenario.inDriveName, scenario.inDataSourceName)
+			}, "scenario '%v' failed. getStorer(%v, %v) should panic!", scenario.desc, scenario.inDriveName, scenario.inDataSourceName)
+		}
 	})
 }
