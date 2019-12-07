8376ca3889d6a970bfb00f4d3932f2414ed202fe
Zealy <ljg_cqu@126.com>
2019-12-07 16:13:17 +0800

    componentized Tracker for nonce, necessary refactoring and testing all done. Tracker now externally depends on *client.Client and *gorms.Gorms as a dot.

--- dots/eth/nonce/storer.go
+++ dots/eth/nonce/storer.go
@@ -7,40 +7,24 @@ import (
 	"database/sql"
 )
 
-// globalStorer is the singleton of interface storer.
-var globalStorer storer
-
-// storer is the interface to interfact with database on nonce storage.
-// Currently it is implemented by type sqlStorer as belowing.
-type storer interface {
-	createAccountIfNotExists(address string, nonce uint64) error
-	deleteAccount(address string) error
-	retrieveNonceAt(address string) (uint64, error)
-	restoreNonce(address string, recycledNonce uint64) error
-	resetNonceAt(address string, nonce uint64) error
-	close() error
+// storer is logically responsible for storage of nonce
+type storer struct {
+	dao *dao
 }
 
-// storer type is logically responsible for manipulation of data hosted in table nonces.
-type sqlStorer struct {
-	driverName, dataSourceName string
-	*sql.DB
-}
-
-// getStorer connects to the database, and then creates the table nonces if neccessary.
-func getStorer(driverName, dataSourceName string) storer {
-	if globalStorer == nil {
-		db := openDB(driverName, dataSourceName)
-		globalStorer = &sqlStorer{driverName, dataSourceName, db}
+// getStorer returns a storer after creating table nonces if it hasn't exist yet.
+func getStorer(db *sql.DB) (*storer, error) {
+	dao := getDao(db)
+	if err := dao.createTableNoncesIFNotExists(); err != nil {
+		return nil, err
 	}
-
-	return globalStorer
+	return &storer{dao}, nil
 }
 
 // Essentially createAccountIfNotExists populates a record into the tables nonces with initial/updated nonce to an address.
 // ErrAccountAlreadyExist will be returned if the accounte has already been created before in storage.
-func (s *sqlStorer) createAccountIfNotExists(address string, nonce uint64) error {
-	count, err := countAllNoncesAt(address)
+func (s *storer) createAccountIfNotExists(address string, nonce uint64) error {
+	count, err := s.dao.countAllNoncesAt(address)
 	if err != nil {
 		return err
 	}
@@ -49,23 +33,23 @@ func (s *sqlStorer) createAccountIfNotExists(address string, nonce uint64) error
 		return ErrAccountAlreadyExist
 	}
 
-	return insertNonce(address, nonce, true)
+	return s.dao.insertNonce(address, nonce, true)
 }
 
 // Essentially deleteAccount removes all records belonging to the given address in the table nonces.
-func (s *sqlStorer) deleteAccount(address string) error {
-    return deleteAllNonces(address)
+func (s *storer) deleteAccount(address string) error {
+	return s.dao.deleteAllNonces(address)
 }
 
 // retrieveNonceAt returns a nonce to the specified address for the next transaction.
 //
 // Pre-condition: the exact address should be populated beforehand via method of createAccountIfNotExists.
-// Post-condition: if there's no recycled nonce restored, the only updated nonce will be returned and increased by one, 
-// otherwise the minimum recycled nonce will be removed from the database and returned.          
-// 
+// Post-condition: if there's no recycled nonce restored, the only updated nonce will be returned and increased by one,
+// otherwise the minimum recycled nonce will be removed from the database and returned.
+//
 // Vialation: in case the address is absent, 0 and ErrNonceNotExist will be returned, meanwhile nothing will be changed in database.
-func (s *sqlStorer) retrieveNonceAt(address string) (uint64, error) {
-	count, err := countAllNoncesAt(address)
+func (s *storer) retrieveNonceAt(address string) (uint64, error) {
+	count, err := s.dao.countAllNoncesAt(address)
 	if err != nil {
 		return 0, err
 	}
@@ -75,9 +59,9 @@ func (s *sqlStorer) retrieveNonceAt(address string) (uint64, error) {
 	}
 
 	if count == 1 {
-		return returnAndIncreaseTheOnlyUpdatedNonce(address)
+		return s.dao.returnAndIncreaseTheOnlyUpdatedNonce(address)
 	}
-	return deleteAndReturnMinRecycledNonce(address)
+	return s.dao.deleteAndReturnMinRecycledNonce(address)
 }
 
 // restoreNonce inserts a record with a recycled nonce into the table nonces for the next transaction.
@@ -90,8 +74,8 @@ func (s *sqlStorer) retrieveNonceAt(address string) (uint64, error) {
 // In case the address is absent in storage, then ErrAccountNotExist will be returned.
 // Otherwise if recycledNonce is less than 0 or bigger than the updatedNonce, ErrInvalidRecycledNonce will be returned.
 // Besides ErrNonceAlreadyExist will be returned if the nonce has been recored in storage.
-func (s *sqlStorer) restoreNonce(address string, recycledNonce uint64) error {
-	ok, err := nonceDoesExist(address, recycledNonce)
+func (s *storer) restoreNonce(address string, recycledNonce uint64) error {
+	ok, err := s.dao.nonceDoesExist(address, recycledNonce)
 	if err != nil {
 		return err
 	}
@@ -99,7 +83,7 @@ func (s *sqlStorer) restoreNonce(address string, recycledNonce uint64) error {
 		return ErrNonceAlreadyExist
 	}
 
-	updatedNonce, err := theOnlyUpdatedNonceAt(address)
+	updatedNonce, err := s.dao.theOnlyUpdatedNonceAt(address)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return ErrAccountNotExist
@@ -111,27 +95,18 @@ func (s *sqlStorer) restoreNonce(address string, recycledNonce uint64) error {
 		return ErrInvalidRecycledNonce
 	}
 
-	return insertNonce(address, recycledNonce, false)
+	return s.dao.insertNonce(address, recycledNonce, false)
 }
 
 // resetNonceAt updates the only updated nonce record to an address with a given new value, and then deletes all the existing recycled records.
 // Note that in case the address is absent, ErrAccountNotExist will be returned.
-func (s *sqlStorer) resetNonceAt(address string, nonce uint64) error {
-	_, err := theOnlyUpdatedNonceAt(address)
+func (s *storer) resetNonceAt(address string, nonce uint64) error {
+	_, err := s.dao.theOnlyUpdatedNonceAt(address)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return ErrAccountNotExist
 		}
 		return err
 	}
-	return resetNonceAt(address, nonce)
+	return s.dao.resetNonceAt(address, nonce)
 }
-
-// close releases the underlying database resource
-func (s *sqlStorer) close() error {
-	defer func(*sqlStorer) {
-		s = nil
-	}(s)
-	return s.Close()
-}
-  
--- dots/eth/nonce/tracker.go
+++ dots/eth/nonce/tracker.go
@@ -4,28 +4,38 @@
 package nonce
 
 import (
-	"github.com/ethereum/go-ethereum/common"
+	"context"
 	"sync"
+
+	"github.com/scryinfo/dot/dot"
+	"github.com/scryinfo/dot/dots/db/gorms"
+	"github.com/scryinfo/dp/dots/eth/client"
+
+	"github.com/ethereum/go-ethereum/common"
+	"go.uber.org/zap"
 )
 
-// myTracker is the singleton of interface Tracker.
-var globalTrack Tracker
+const (
+	TrackerTypeId = "15fa392f-042e-43f4-9df0-934bdb0fcf78"
+)
 
 // Tracker stands for the interface to mainly retrieve and restore nonce with the database for transactions onto Ethereum.
 //
 // Callers must create an account with an address to make sure the Tracker can function well. When creating an account
 // its nonce will be synchronized with the Ethereum network simultaneously and silently, and henceforth the nonce to an address
 // will be guarded by the database. This means that callers don't have to request the Ethereum network for nonces each time they
-// want to send transactions. 
+// want to send transactions.
 //
-// Normally callers can happily obtain a nonce via method RetrieveNonceAt and then the cource of corresponding transaction should go smoothly. 
+// Normally callers can happily obtain a nonce via method RetrieveNonceAt and then the cource of corresponding transaction should go smoothly.
 //
-// But in rare case that a retrieved nonce unexpectedly fails, the caller must return it back via method RestoreNonce for recycle, 
-// which is vital for the Tracker to keep working fine. We say a nonce fails when it has been retrieved from the database 
+// But in rare case that a retrieved nonce unexpectedly fails, the caller must return it back via method RestoreNonce for recycle,
+// which is vital for the Tracker to keep working fine. We say a nonce fails when it has been retrieved from the database
 // but eventually fails being sent into the TxPool of the Ethereum network.
 //
 // In a worst-case scenario the Tracker happens to generates inappropriate nonces and thus leads to failure of transactions even under normal use,
-// the caller can call ResetNonceAt to re-synchronize a nonce with the Ethereum network at the right time and thus to keep data in consistency.
+// the caller can call ResetNonceAt to re-synchronize a nonce with the Ethereum network at the right time and thus to keep dao in consistency.
+//
+// Tracker is designed to be safe for concurrent use by multiple goroutines.
 type Tracker interface {
 	CreateAccount(address string) error
 	DeleteAccount(address string) error
@@ -34,59 +44,122 @@ type Tracker interface {
 	RestoreNonce(address string, recycledNonce uint64) error
 
 	ResetNonceAt(address string) error
-
-	Close() error
 }
 
-// SQLTracker is an implementation of interface Tracker interface through interacting with RDBS
-type SQLTracker struct {
-	mu             *sync.Mutex
-	driverName     string
-	dataSourceName string
-	ethNodeURL     string
-	storer         storer
-	client         client
+// TrackerImp implements Tracker interface, with component dependencies of *gorms.Gorms and *client.Client,
+// which must be configured correctly by callers beforehand in order to get TrackerImp work normally as expected.
+type TrackerImp struct {
+	mu *sync.Mutex
+
+	ClientDot *client.Client `dot:"bee6ae8e-e8a0-4693-ac65-416f618023d5"` // "github.com/scryinfo/dp/dots/eth/client"
+	GormsDot  *gorms.Gorms   `dot:"d2b575cd-e38f-4002-b4bd-9dc85fe13fe6"` // "github.com/scryinfo/dot/dots/db/gorms"
+
+	ethClient ethClient // depended to fetch and synchronize nonce from Ethereum network
+	storer    *storer   // depended to storer and guard nonce off Ethereum network
 }
 
-// GetTracker return a tracker with the given driverName, dataSourceName and ethNodeURL.
-// The returned Traker is safe for concurrent use by multiple goroutines.
-func GetTracker(driverName, dataSourceName, ethNodeURL string) Tracker {
-	if globalTrack == nil {
-		globalTrack = &SQLTracker{
-			mu:             &sync.Mutex{},
-			driverName:     driverName,
-			dataSourceName: dataSourceName,
-			ethNodeURL:     ethNodeURL,
-			storer:         getStorer(driverName, dataSourceName),
-			client:         getClient(ethNodeURL),
-		}
+// ethClient is an interface wrapper around *ethclient.Client.
+type ethClient interface {
+	PendingNonceAt(ctx context.Context, account common.Address) (uint64, error)
+}
+
+// TrackerLive returns TypeLives for generating TrackerImp dot.
+func TrackerLive() *dot.TypeLives {
+	newTracker := func(conf []byte) (dot.Dot, error) {
+		return &TrackerImp{mu: &sync.Mutex{}}, nil
 	}
-	return globalTrack
+
+	return &dot.TypeLives{
+		Meta: dot.Metadata{TypeId: TrackerTypeId, NewDoter: newTracker},
+	}
+}
+
+//TrackerLiveAndDep returns TypeLives for generating TrackerImp and its dependent dots.
+func TrackerLiveAndDep() []*dot.TypeLives {
+	newTracker := func(conf []byte) (dot.Dot, error) {
+		return &TrackerImp{mu: &sync.Mutex{}}, nil
+	}
+
+	return []*dot.TypeLives{
+		&dot.TypeLives{
+			Meta: dot.Metadata{TypeId: TrackerTypeId, NewDoter: newTracker},
+		},
+		client.ClientTypeLive(),
+		gorms.TypeLives()[0],
+	}
+}
+
+// Create injects ClientDot, GormsDot as well as unexported ethClient and storer,
+// basing on communicating with the given dot line.
+func (t *TrackerImp) Create(l dot.Line) error {
+	logger := dot.Logger()
+
+	// configure ClientDot
+	clientDot, err := l.ToInjecter().GetByLiveId(client.ClientTypeId)
+	if err != nil {
+		logger.Errorln("TrackerImp dotGetByLiveId", zap.Error(err))
+		return err
+	}
+
+	t.ClientDot = clientDot.(*client.Client)
+
+	// configure GormsDot
+	gormsDot, err := l.ToInjecter().GetByLiveId(gorms.TypeId)
+	if err != nil {
+		logger.Errorln("TrackerImp dotGetByLiveId", zap.Error(err))
+		return err
+	}
+	t.GormsDot = gormsDot.(*gorms.Gorms)
+
+	// create ethClient
+	t.ethClient = t.ClientDot.EthClient()
+
+	// create storer
+	if err := createStorer(t); err != nil {
+		logger.Errorln("TrackerImp initStorer", zap.Error(err))
+		return err
+	}
+	return nil
+}
+
+// createStorer creates a storer for the given TrackerImp.
+// Under the hook table nonces will be created if it doesn't exist yet.
+var createStorer = func(t *TrackerImp) error {
+	db := t.GormsDot.Db.DB()
+	if err := db.Ping(); err != nil {
+		return err
+	}
+	storer, err := getStorer(db)
+	if err != nil {
+		return err
+	}
+	t.storer = storer
+	return nil
 }
 
 // CreateAccount creates an account to the given address if it does not exist yet
 // with an initial/updated pending nonce, which is synchronized from the Ethereum network.
 // CreateAccount will return ErrInvalidAddress when the given address is not a valid hex-encoded Ethereum address,
 // and will return ErrAccountAlreadyExist if the account has already been created before.
-func (t *SQLTracker) CreateAccount(address string) error {
+func (t *TrackerImp) CreateAccount(address string) error {
 	t.mu.Lock()
 	defer t.mu.Unlock()
 
 	if !common.IsHexAddress(address) {
 		return ErrInvalidAddress
 	}
-	
-	nonce, err := t.client.pendingNonceAt(address); 
+
+	nonce, err := t.pendingNonceAt(address)
 	if err != nil {
 		return err
 	}
-	
+
 	return t.storer.createAccountIfNotExists(address, nonce)
 }
 
 // DeleteAccount deletes the given account and its associated nonces from the database.
 // It won't throw an error when the address is absent in storage.
-func (t *SQLTracker) DeleteAccount(address string) error {
+func (t *TrackerImp) DeleteAccount(address string) error {
 	t.mu.Lock()
 	defer t.mu.Unlock()
 	return t.storer.deleteAccount(address)
@@ -96,15 +169,15 @@ func (t *SQLTracker) DeleteAccount(address string) error {
 //
 // Indeed the returned nonce maybe a newly updated one or just a recycled one, which is fully reflected in storage.
 // Usually callers needn't care about what status a returned nonce is, which are equivalent for a successful transaction.
-// 
+//
 // Thus RetrieveNonceAt provides the pure value of nonce that can be used directly for the next transaction,
-// and callers are free from being annoyed by redundant information on nonce status. 
-// 
+// and callers are free from being annoyed by redundant information on nonce status.
+//
 // Note that in case the address is absent, 0 and ErrNonceNotExist will be returned.
-func (t *SQLTracker) RetrieveNonceAt(address string) (nonce uint64, err error) {
+func (t *TrackerImp) RetrieveNonceAt(address string) (nonce uint64, err error) {
 	t.mu.Lock()
 	defer t.mu.Unlock()
-	
+
 	return t.storer.retrieveNonceAt(address)
 }
 
@@ -114,7 +187,7 @@ func (t *SQLTracker) RetrieveNonceAt(address string) (nonce uint64, err error) {
 // In case the address is absent in storage, then ErrAccountNotExist will be returned.
 // Otherwise if recycledNonce is less than 0 or bigger than the updatedNonce, ErrInvalidRecycledNonce will be returned.
 // Besides ErrNonceAlreadyExist will be returned if the nonce has been recored in storage.
-func (t *SQLTracker) RestoreNonce(address string, recycledNonce uint64) error {
+func (t *TrackerImp) RestoreNonce(address string, recycledNonce uint64) error {
 	t.mu.Lock()
 	defer t.mu.Unlock()
 	return t.storer.restoreNonce(address, recycledNonce)
@@ -122,24 +195,21 @@ func (t *SQLTracker) RestoreNonce(address string, recycledNonce uint64) error {
 
 // ResetNonceAt refreshes the nonce to the given address in storage by synchronizing with the Ethereum network.
 // Note that in case the address is absent in storage, ErrAccountNotExist will be returned.
-func (t *SQLTracker) ResetNonceAt(address string) error {
+func (t *TrackerImp) ResetNonceAt(address string) error {
 	t.mu.Lock()
 	defer t.mu.Unlock()
-	
-	nonce, err := t.client.pendingNonceAt(address); 
+
+	nonce, err := t.pendingNonceAt(address)
 	if err != nil {
 		return err
 	}
-	
+
 	return t.storer.resetNonceAt(address, nonce)
 }
 
-// Close releases all underlining resources, namely the database and ethclient connections.
-// It is rarely necessary to close a Tracker.
-func (t *SQLTracker) Close() error {
-	t.mu.Lock()
-	defer t.mu.Unlock()
-
-	t.client.close()
-	return  t.storer.close()
+// pendingNonce returns nonce of the given account at pending state.
+// This is the nonce that should be used for the next transaction.
+// Note that in case of invalid address, 0 and nil will be returned.
+func (t *TrackerImp) pendingNonceAt(address string) (uint64, error) {
+	return t.ethClient.PendingNonceAt(context.Background(), common.HexToAddress(address))
 }
