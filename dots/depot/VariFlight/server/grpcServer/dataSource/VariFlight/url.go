61a40e3e1f899da6c5b2a69392aa038e3caf2548
Zealy <ljg_cqu@126.com>
2020-03-03 05:03:34 +0800

    go fmt url.go

--- dots/depot/VariFlight/server/grpcServer/dataSource/VariFlight/url.go
+++ dots/depot/VariFlight/server/grpcServer/dataSource/VariFlight/url.go
@@ -1,3 +1,6 @@
+// Scry Info.  All rights reserved.
+// license that can be found in the license file.
+
 package VariFlight
 
 import (
@@ -8,43 +11,43 @@ import (
 )
 
 // url returns a url with corresponding _token that should be used to request the API provided by http://www.variflight.com/.
-func url(urlWithoutQueryParams string, queryParameters map[string]string, registrationCode string) string {
-	// join the query parameters with the registration code, and get a query string with corresponding _token.
-	queryStrWithToken := queryWithToken(queryParameters, registrationCode)
+func url(urlWithoutQueryParams string, queryParameters map[string]string, appsecurity string) string {
+	// join the query parameters with the appsecurity, and get a query string with corresponding _token.
+	queryStrWithToken := queryWithToken(queryParameters, appsecurity)
 	// join the query string with the front part of url to conform the final whole requested url.
 	return urlWithoutQueryParams + queryStrWithToken
 }
 
-// _token returns a _token that generated from the given queryParamsMap and registrationCode.
-func token(queryParamsMap map[string]string, registrationCode string) string {
+// _token returns a _token that generated from the given queryParamsMap and appsecurity.
+func token(queryParamsMap map[string]string, appsecurity string) string {
 	// convert query parameters map to required string, whose parameter names are made lowercase and parameters are
 	// sorted in increasingly order and are concatenated with "&".
 	paramsSortedStr := convertMapToStr(queryParamsMap)
 
-	// join the registration code
-	paramsSortedStrWithRegCode := paramsSortedStr + "&appsecurity=" + registrationCode
+	// join the appsecurity
+	paramsSortedStrWithRegCode := paramsSortedStr + "&appsecurity=" + appsecurity
 
 	// encrypt twice by MD5
 	return md5md5(paramsSortedStrWithRegCode)
 }
 
 //  queryWithToken returns a query string that carries a corresponding _token.
-func queryWithToken(queryParameters map[string]string, registrationCode string) string {
+func queryWithToken(queryParameters map[string]string, appsecurity string) string {
 	// convert query parameters map to required string, whose parameter names are made lowercase and parameters are
 	// sorted in increasingly order and are concatenated with "&".
 	paramsSortedStr := convertMapToStr(queryParameters)
 
-	// generate corresponding _token with the given paramsSortedStr and registrationCode
-	token := _token(paramsSortedStr, registrationCode)
+	// generate corresponding _token with the given paramsSortedStr and appsecurity
+	token := _token(paramsSortedStr, appsecurity)
 
 	// join the paramsSortedStr with _token
 	return paramsSortedStr + "&_token=" + token
 }
 
-// _token returns a _token that generated from the given paramsSortedStr and registrationCode.
-func _token(paramsSortedStr string, registrationCode string) string {
-	// join the registration code
-	paramsSortedStrWithRegCode := paramsSortedStr + "&appsecurity=" + registrationCode
+// _token returns a _token that generated from the given paramsSortedStr and appsecurity.
+func _token(paramsSortedStr string, appsecurity string) string {
+	// join the appsecurity
+	paramsSortedStrWithRegCode := paramsSortedStr + "&appsecurity=" + appsecurity
 
 	// encrypt twice
 	return md5md5(paramsSortedStrWithRegCode)
