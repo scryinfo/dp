b7cac255f329fe7dd671d1839f10cc1659f0ed4a
Zealy <ljg_cqu@126.com>
2020-03-06 16:18:17 +0800

    remove spew import

--- dots/depot/VariFlight/server/VariFlightServer.go
+++ dots/depot/VariFlight/server/VariFlightServer.go
@@ -5,7 +5,7 @@ import (
 
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
-	"github.com/davecgh/go-spew/spew"
+	"github.com/davecgh/go-spew/fmt"
 
 	"github.com/scryinfo/dot/dot"
 	"github.com/scryinfo/dot/dots/grpc/gserver"
@@ -47,31 +47,31 @@ func VariFlightServerTypeLives() []*dot.TypeLives {
 						os.Exit(1)
 					}
 					dot.Logger().Debug(func() string {
-						return spew.Sprintf("VariFlightServerConfig: %#+v", _conf)
+						return fmt.Sprintf("VariFlightServerConfig: %v", _conf)
 					})
 					return &VariFlightServer{config: &_conf}, nil
 				},
 			},
-			Lives: []dot.Live{
-				{
-					TypeId: VariFlightServerTypeId,
-					RelyLives: map[string]dot.LiveId{
-						"VariFlightApiCaller": VariFlight.VariFlightApiCallerTypeId,
-						"WebSocket":        gserver.WebSocketTypeId,
-					},
-				},
-			},
+			//Lives: []dot.Live{
+			//	{
+			//		TypeId: VariFlightServerTypeId,
+			//		RelyLives: map[string]dot.LiveId{
+			//			"VariFlightApiCaller": VariFlight.VariFlightApiCallerTypeId,
+			//			"WebSocket":        gserver.WebSocketTypeId,
+			//		},
+			//	},
+			//},
 		},
 	}
 
 	typeLives = append(typeLives, VariFlight.VariFlightCallerTypeLives()...)
-	typeLives = append(typeLives, gserver.WebSocketAndRelyTypeLives()...)
+	typeLives = append(typeLives, gserver.WebSocketTypeLives()...)
 	return typeLives
 }
 
 func (s *VariFlightServer) AfterAllInject(l dot.Line) {
 	s.grpcServer = grpc.NewServer()
-	s.serviceServer = new(s.VariFlightApiCaller)
+	s.serviceServer = newVariFlightServiceServer(s.VariFlightApiCaller)
 	_proto.RegisterVariFlightDataServiceServer(s.grpcServer, s.serviceServer)
 
 	s.WebSocket.GET(s.config.ServicePath, s.grpcServer)
